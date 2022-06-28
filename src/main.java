9dac498ba9b972393557abf28e9592bdbfbb4735
Artem <artemkhrychov@gmail.com>
2022-06-28 16:45:23 +0300

    New user in main and renamed model1.java  into User.java

--- src/main.java
+++ src/main.java
@@ -1,10 +1,11 @@
-import models.model1;
+import models.User;
 
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("My name");
 
-        model1 a = new model1("Artem", "Khrychov");
+        User a = new User("Artem", "Khrychov");
+        User b = new User("Denis", "Khrychov");
     }
 }
--- src/models/User.java
+++ src/models/User.java
@@ -1,10 +1,10 @@
 package models;
 
-public class model1 {
+public class User {
     String name;
     String surname;
 
-    public model1(String name, String surname) {
+    public User(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
