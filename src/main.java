4754e6ff006f7d895c623197c5a7fc5c7e60ff37
Bob <bob@gmail.com>
2022-06-28 17:07:24 +0300

    Added print Hello Artem

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ import models.User;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello Artem");
         System.out.println("My name");
 
         User a = new User("Artem", "Khrychov", "a");
