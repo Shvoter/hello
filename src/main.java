7210544d3985ff74313834f1ae23c2f19388cf74
Bob <bob@gmail.com>
2022-06-28 16:27:56 +0300

    Addad model1

--- src/main.java
+++ src/main.java
@@ -1,6 +1,10 @@
+import models.model1;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("My name");
+
+        model1 a = new model1("Artem", "Khrychov");
     }
 }
---
+++ src/models/model1.java
@@ -0,0 +1,11 @@
+package models;
+
+public class model1 {
+    String name;
+    String surname;
+
+    public model1(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
